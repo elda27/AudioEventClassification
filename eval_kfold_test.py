11ae80b0a0877fa6eb3f2db6b62dc1840fc09173
elda27 <kaz.birdstick@gmail.com>
2020-10-12 01:50:06 +0900

    Update all

---
+++ eval_kfold_test.py
@@ -0,0 +1,42 @@
+import argparse
+from pathlib import Path
+from tqdm import trange, tqdm
+import _pickle as pickle
+import numpy as np
+
+networks = {
+    'cnn': 'cnn_cnn',
+    'cnn-resnet': 'cnn_resnet'
+}
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--network', choices=list(networks.keys()), required=True)
+    parser.add_argument('--task', type=str, required=True)
+    args = parser.parse_args()
+
+    prefix = networks[args.network]
+
+    model_dir = Path('model')
+    agg_df = None
+    for fold_index in trange(4):
+        metric_dir = model_dir / f'{args.task}_{prefix}_fold{fold_index}'
+        with open(metric_dir / 'result_metric.pickle', 'rb') as fp:
+            metrics = pickle.load(fp)
+            if agg_df is None:
+                agg_df = metrics['test']['Agg']
+            else:
+                agg_df += metrics['test']['Agg']
+
+    metric_dir = Path('metric') / f'{args.task}_{prefix}'
+    metric_dir.mkdir(exist_ok=True, parents=True)
+    agg_df['Accuracy'] = np.array([
+        (agg_df.iloc[i, i] / agg_df.iloc[:, i].sum())
+        for i in range(len(agg_df))
+    ])
+    agg_df.to_html(metric_dir / 'ConfusionMatrix.html')
+
+
+if __name__ == "__main__":
+    main()
---
+++ run_kfold_test.py
@@ -0,0 +1,47 @@
+import papermill
+import argparse
+from pathlib import Path
+from tqdm import trange, tqdm
+
+networks = {
+    'cnn': {
+        'model_type': 'cnn',
+        'file': 'CNN.ipynb',
+    },
+    'cnn-resnet': {
+        'model_type': 'resnet',
+        'file': 'CNN.ipynb',
+    }
+}
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--network', type=str,
+                        choices=list(networks.keys()), required=True)
+    parser.add_argument('--task', type=str, required=True)
+    args = parser.parse_args()
+
+    params = networks[args.network]
+    file = params.pop('file')
+    common_params = {
+        'task': args.task,
+    }
+    common_params.update(params)
+
+    name = Path(file).stem.lower()
+    for fold_index in trange(4):
+        params = {
+            'fold_index': fold_index,
+        }
+        params.update(common_params)
+        papermill.execute_notebook(
+            file,
+            str(Path('model') /
+                f'{params["model_type"]}_{name}_fold{fold_index}.ipynb'),
+            parameters=params
+        )
+
+
+if __name__ == "__main__":
+    main()
